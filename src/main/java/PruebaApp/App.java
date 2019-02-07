beaa20604b203bf6e193d19128c4f7c0a4c66ff1
pradeep <pradeep@yahoo.com>
2019-02-07 07:56:53 +0530

    File added

--- src/main/java/PruebaApp/App.java
+++ src/main/java/PruebaApp/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! first time");
+        System.out.println( "Hello World! to display on the screen");
         
     }
 }
